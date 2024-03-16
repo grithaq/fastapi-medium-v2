1f8d4ed1ee343be0f48a52659324f4e06dd866fe
grithaq <afifnurazizi@gmail.com>
2024-03-16 22:10:00 +0800

    feat: create get category endpoint

--- src/crud/category_crud.py
+++ src/crud/category_crud.py
@@ -12,14 +12,14 @@ class CRUDCategory(CRUDBase[Category, CategoryCreate, CategoryUpdate]):
     def create(
         self, db: Session, obj_in: CategoryCreate, user_id: int
     ) -> Optional[Category]:
-        db_obj = Category(id=obj_in.id, name=obj_in.name, user_id=user_id)
+        db_obj = Category(obj_in.model_dump(), user_id=user_id)
         db.add(db_obj)
         db.commit()
         db.refresh(db_obj)
         return db_obj
 
-    def get_categories(self, db: Session):
-        categories = db.query(Category).all()
+    def get(self, db: Session, user_id: int) -> Optional[Category]:
+        categories = db.query(Category).filter(Category.user_id == user_id).all()
         return categories
 
 
