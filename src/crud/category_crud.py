40f82ed1f3364b778ede6b9d9a0b64b494fe5152
grithaq <afifnurazizi@gmail.com>
2024-03-19 13:38:02 +0800

    reformat file using pre-commit #30

--- src/crud/category_crud.py
+++ src/crud/category_crud.py
@@ -3,7 +3,7 @@ from typing import Optional
 from sqlalchemy.orm import Session
 
 from db.models import Category
-from schemas import CategoryCreate, CategoryUpdate, CategoryRequest
+from schemas import CategoryCreate, CategoryUpdate
 
 from .base import CRUDBase
 
@@ -21,9 +21,16 @@ class CRUDCategory(CRUDBase[Category, CategoryCreate, CategoryUpdate]):
     def get(self, db: Session, user_id: int) -> Optional[Category]:
         categories = db.query(Category).filter(Category.user_id == user_id).all()
         return categories
-    
-    def update(self, db: Session, obj_in: CategoryCreate, user_id: int) -> Optional[Category]:
-        category = db.query(Category).filter(Category.user_id == user_id).filter(Category.id == obj_in.id).first()
+
+    def update(
+        self, db: Session, obj_in: CategoryCreate, user_id: int
+    ) -> Optional[Category]:
+        category = (
+            db.query(Category)
+            .filter(Category.user_id == user_id)
+            .filter(Category.id == obj_in.id)
+            .first()
+        )
         if category is not None:
             category.name = obj_in.name
             category.user_id = user_id
